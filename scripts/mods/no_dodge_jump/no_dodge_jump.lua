8a8bb465e9dca051981b1de07cd41c290ec6e34c
jayemen <jayemen.85@gmail.com>
2023-11-10 21:23:00 -0500

    Change approach

--- scripts/mods/no_dodge_jump/no_dodge_jump.lua
+++ scripts/mods/no_dodge_jump/no_dodge_jump.lua
@@ -1,39 +1,16 @@
 local mod = get_mod("no_dodge_jump")
-local dodge_require_path = "scripts/extension_systems/character_state_machine/character_states/player_character_state_dodging"
-local walk_require_path = "scripts/extension_systems/character_state_machine/character_states/player_character_state_walking"
+local input_service_path = "scripts/managers/input/input_service"
 
-mod:hook_require(dodge_require_path, function(instance) 
-    mod:hook(instance, "fixed_update", function(func, self, ...)
-        local sideways = self._input_extension:get("move").x ~= 0.0
-        local backwards = self._input_extension:get("move").y < 0.0
-        local dodge_component = self._dodge_character_state_component
-        
-        if not sideways and not backwards then
-            return func(self, ...)
-        else
-            local jump_override_time = dodge_component.jump_override_time
-            dodge_component.jump_override_time = 0
-            local ret = func(self, ...)
-            dodge_component.jump_override_time = jump_override_time
-            return ret
+mod:hook_require(input_service_path, function(instance)
+    mod:hook(instance, "_get", function(func, self, action_name, ...)
+        if action_name ~= "jump" then
+            return func(self, action_name, ...)
         end
-    end)
-end)
-
-mod:hook_require(walk_require_path, function(instance) 
-    mod:hook(instance, "fixed_update", function(func, self, ...)
-        local sideways = self._input_extension:get("move").x ~= 0.0
-        local backwards = self._input_extension:get("move").y < 0.0
-        local movement_state_component = self._movement_state_component
         
-        if not sideways and not backwards then
-            return func(self, ...)
-        else
-            local can_jump = movement_state_component.can_jump
-            movement_state_component.can_jump = false
-            local ret = func(self, ...)
-            movement_state_component.can_jump = can_jump
-            return ret
-        end
+        local jump = func(self, "jump")
+        local back = func(self, "move_backward") ~= 0
+        local left = func(self, "move_left") ~= 0
+        local right = func(self, "move_right") ~= 0
+        return jump and not back and not left and not right
     end)
 end)
